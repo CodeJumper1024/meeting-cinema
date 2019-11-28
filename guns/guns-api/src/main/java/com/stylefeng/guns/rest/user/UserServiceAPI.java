f1d7c0f38d7e957b62bde991a4082c4e871d3d1c
gaowei <gavinkoh7@gmail.com>
2019-11-28 21:56:59 +0800

    commited by gaowei

---
+++ guns/guns-api/src/main/java/com/stylefeng/guns/rest/user/UserServiceAPI.java
@@ -0,0 +1,7 @@
+package com.stylefeng.guns.rest.user;
+
+import com.stylefeng.guns.rest.user.vo.UserInfoVo;
+
+public interface UserServiceAPI {
+    UserInfoVo updateUserInfo(UserInfoVo userInfoVo);
+}
