612c6a92ec0800158f1955373259bce78170fdd6
CodeJumper <xudongdong1216@163.com>
2019-11-28 21:08:33 +0800

    created by xudongdong 19-11-28 21:08

---
+++ guns/guns-film/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MtimeBannerTMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.common.persistence.dao;
+
+import com.stylefeng.guns.rest.common.persistence.model.MtimeBannerT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * banner信息表 Mapper 接口
+ * </p>
+ *
+ * @author xdd
+ * @since 2019-11-28
+ */
+public interface MtimeBannerTMapper extends BaseMapper<MtimeBannerT> {
+
+}
