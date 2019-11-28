a21e4c10c069f5a47721f0bffe0568f5fba72eb1
CodeJumper <xudongdong1216@163.com>
2019-11-28 17:28:04 +0800

    created by xudongdong 19-11-28 17:28

--- guns/guns-api/src/main/java/com/stylefeng/guns/rest/film/vo/BaseReqVo.java
+++ guns/guns-api/src/main/java/com/stylefeng/guns/rest/film/vo/BaseReqVo.java
@@ -4,5 +4,13 @@ import lombok.Data;
 
 @Data
 public class BaseReqVo<T> {
-
+    T data;
+    String msg;
+    Integer status;
+    public static BaseReqVo ok(){
+        BaseReqVo baseReqVo = new BaseReqVo();
+        baseReqVo.setStatus(0);
+        baseReqVo.setMsg("成功");
+        return baseReqVo;
+    }
 }
