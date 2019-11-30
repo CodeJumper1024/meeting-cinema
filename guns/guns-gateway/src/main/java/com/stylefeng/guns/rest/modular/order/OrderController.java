10228d079a28f2a917e6d2f9b6e364d9bc5028d6
gaowei <gavinkoh7@gmail.com>
2019-11-30 17:10:01 +0800

    commited by gaowei

---
+++ guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/order/OrderController.java
@@ -0,0 +1,14 @@
+package com.stylefeng.guns.rest.modular.order;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.stylefeng.guns.rest.order.OrderService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("order")
+public class OrderController {
+
+    @Reference(interfaceClass = OrderService.class, check = false)
+    OrderService orderService;
+}
