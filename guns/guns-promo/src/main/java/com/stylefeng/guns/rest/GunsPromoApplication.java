ab4fcf65a8d08b506b003f0f08f03415d27b57b9
unknown <malei95324@163.com>
2019-12-03 16:36:49 +0800

    malei

--- guns/guns-promo/src/main/java/com/stylefeng/guns/rest/GunsPromoApplication.java
+++ guns/guns-promo/src/main/java/com/stylefeng/guns/rest/GunsPromoApplication.java
@@ -1,12 +1,14 @@
 package com.stylefeng.guns.rest;
 
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.stylefeng.guns"})
-public class GunsRestApplication {
+@EnableDubboConfiguration
+public class GunsPromoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GunsRestApplication.class, args);
+        SpringApplication.run(GunsPromoApplication.class, args);
     }
 }
